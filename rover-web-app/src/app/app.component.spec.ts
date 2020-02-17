20689fcdb21f0a7e8ca4420c69cc41973b5b1280
Kyle Preston <kyle_preston@apple.com>
2020-02-17 11:24:30 -0600

    updating tests for service changes

--- rover-web-app/src/app/app.component.spec.ts
+++ rover-web-app/src/app/app.component.spec.ts
@@ -4,7 +4,6 @@ import { HttpClientModule } from '@angular/common/http';
 import {BrowserModule} from "@angular/platform-browser";
 import {AppRoutingModule} from "./app-routing.module";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {RoverGridComponent} from "./rover-grid/rover-grid.component";
 
 describe('AppComponent', () => {
   let app: AppComponent;
--- rover-web-app/src/app/rover.service.spec.ts
+++ rover-web-app/src/app/rover.service.spec.ts
@@ -1,16 +1,34 @@
-import { TestBed } from '@angular/core/testing';
+import {async, TestBed} from '@angular/core/testing';
 
 import { RoverService } from './rover.service';
+import {BrowserModule} from "@angular/platform-browser";
+import {AppRoutingModule} from "./app-routing.module";
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {AppComponent} from "./app.component";
 
 describe('RoverService', () => {
   let service: RoverService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientModule,
+      ],
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
     service = TestBed.inject(RoverService);
-  });
+  }));
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('initializeRoverGrid should create 5x5 array', () => {
+    const roverGrid = service.initializeRoverGrid(5);
+    expect(roverGrid.length == 5).toBeTruthy();
+    expect(roverGrid[0].length == 5).toBeTruthy();
   });
 });
