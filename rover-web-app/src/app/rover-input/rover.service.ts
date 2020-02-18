a7b18199c11e33b898e985f75d19ab140a241dac
Kyle Preston <kyle_preston@apple.com>
2020-02-18 10:39:00 -0600

    Update

--- rover-web-app/src/app/rover-input/rover.service.ts
+++ rover-web-app/src/app/rover-input/rover.service.ts
@@ -13,7 +13,7 @@ export class RoverService {
     let headers = new HttpHeaders();
     headers.set('Access-Control-Allow-Origin', '*');
     headers.set('content-type', 'application/json');
-    return this.http.get(`${this.roverUrl}/location/${initialPosition}/commands/${commands}`, {headers})
+    return this.http.get(`${this.roverUrl.toUpperCase()}/location/${initialPosition.toUpperCase()}/commands/${commands}`, {headers})
   }
 
   initializeRoverGrid(dimension: number) {
